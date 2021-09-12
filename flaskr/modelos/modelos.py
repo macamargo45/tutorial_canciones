76739e5294da33e5b9553a5dbc32298759351495
Miguel Camargo <ma.camargo45@uniandes.edu.co>
2021-09-12 01:06:38 -0500

    CReating the relationship in the moodels

--- flaskr/modelos/modelos.py
+++ flaskr/modelos/modelos.py
@@ -3,36 +3,39 @@ import enum
 
 db = SQLAlchemy()
 
-class Cancion(db.Model):
+albums_songs = db.Table('album_song',
+    db.Column('album_id', db.Integer, db.ForeignKey('album.id'), primary_key = True),
+    db.Column('song_id', db.Integer, db.ForeignKey('song.id'), primary_key = True))
+
+class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(128))
-    minutos = db.Column(db.Integer)
-    segundos = db.Column(db.Integer)
-    interprete = db.Column(db.String(128))
+    title = db.Column(db.String(128))
+    minutes = db.Column(db.Integer)
+    seconds = db.Column(db.Integer)
+    artist = db.Column(db.String(128))
+    albums = db.relationship('Album', secondary = 'album_song', back_populates="songs")
 
     def __repr__(self):
-        return "{} - {} - {} - {}".format(self.titulo, self.minutos, self.segundos, self.interprete)
+        return "{} - {} - {} - {}".format(self.title, self.minutes, self.seconds, self.artist)
 
-class Usuario(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nombre_usuario = db.Column(db.String(128))
-    contrasena = db.Column(db.String(128))
+    name = db.Column(db.String(128))
+    password = db.Column(db.String(128))
+    albums = db.relationship('Album', cascade='all, delete, delete-orphan')
 
-    def __repr__(self):
-        return "{} - {} - {} - {}".format(self.nombre_usuario, self.contrasena)
-
-class Medio(enum.Enum):
-   DISCO = 1
-   CASETE = 2
+class Medium(enum.Enum):
+   DISC = 1
+   CASSETE = 2
    CD = 3
 
 class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    titulo = db.Column(db.String(128))
-    ano = db.Column(db.Integer)
-    descripcion = db.Column(db.String(128))
-    medio = db.Column(db.Enum(Medio))
-
-    def __repr__(self):
-        return "{} - {} - {} - {}".format(self.nombre_usuario, self.contrasena)
+    title = db.Column(db.String(128))
+    year = db.Column(db.Integer)
+    description = db.Column(db.String(128))
+    medium = db.Column(db.Enum(Medium))
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    songs = db.relationship('Song', secondary = 'album_song', back_populates="albums")
+    __table_args__ = (db.UniqueConstraint('user', 'title', name='unique_album_title'),)
 
